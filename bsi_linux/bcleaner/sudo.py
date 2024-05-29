250a365132318558a0f3b29cfeb3a1cb30815f2b
PrezidentB <biojoutd@gmail.com>
2024-05-29 18:23:29 +0200

    Finally ready ?

--- bsi_linux/bcleaner/sudo.py
+++ bsi_linux/bcleaner/sudo.py
@@ -1,9 +1,6 @@
 import os
 import logging
 
-# Logging configuration
-logging.basicConfig(filename='./bcleaner/logs/bcleaner.log', filemode='w', format='%(asctime)s - %(message)s' ,level=logging.INFO)
-
 def get_euid():
     # Getting username and home directory
     euid = os.geteuid()
@@ -12,5 +9,5 @@ def get_euid():
     if euid == 0:
         # Running in root might be dangerous, so we raise an error
         raise PermissionError("You might want to run this script normally, not as sudo.")
-    logging.info(f"Running as : {username} with home directory: {home}")
+    print(f"Running as : {username} with home directory: {home}")
     return home
